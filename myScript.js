50153556b3a55e7a1d04969667da1db6bb5866ba
Gabe <gabriel.aj.raffa@gmail.com>
2022-07-11 18:51:50 -0500

    adjustments to be made

--- myScript.js
+++ myScript.js
@@ -1,9 +1,10 @@
 drawArea = document.querySelector("#container")
 
 /* Creates a square of divs with the given length */
-function makeGrid (num) {
+function makeGrid(num) {
     for(let i = 0; i < Math.pow(num, 2); i++) {
         let div = document.createElement("div");
+        div.setAttribute("class", "grid-square");
         drawArea.appendChild(div);
     }
-}
+};
