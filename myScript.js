8b8b5f919fa59935d3dbbd433035bd6a03ba01c3
Gabe <gabriel.aj.raffa@gmail.com>
2022-07-12 21:36:31 -0500

    grid ready

--- myScript.js
+++ myScript.js
@@ -5,6 +5,9 @@ function makeGrid(num) {
     for(let i = 0; i < Math.pow(num, 2); i++) {
         let div = document.createElement("div");
         div.setAttribute("class", "grid-square");
+        div.style.width = `${Math.floor((drawArea.offsetWidth - 2)/num) - 2}px`;
+        //div.style.width = `${100/num}%`;
+        div.style.height = div.style.width;
         drawArea.appendChild(div);
     }
 };
